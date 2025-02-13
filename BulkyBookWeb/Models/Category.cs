4186d347e112723c3eb816804a7486ef7f2d0b00
Ronsell Fortu <ronsell42@gmail.com>
2025-02-14 05:30:57 +0800

    Initial commit

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key] // Data Annotation
+        public int Id { get; set; }
+        [Required] // Data Annotation
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
